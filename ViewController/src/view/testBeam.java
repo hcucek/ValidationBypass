963f6c4c38d918641e37a8a3a4612b7f06bde6fc
hcucek <hrvoje.cucek@neos.hr>
2016-10-17 15:04:11 +0200

    Dodan log na ekran da nije potrebno paralelno gledati konzolu.

--- ViewController/src/view/testBeam.java
+++ ViewController/src/view/testBeam.java
@@ -9,6 +9,9 @@ import oracle.adf.view.rich.render.ClientEvent;
 import org.apache.myfaces.trinidad.component.UIXCommand;
 
 public class testBeam implements Serializable {
+    
+    String natpis = new String();
+    StringBuilder log = new StringBuilder();
     public testBeam() {
     }
 
@@ -19,9 +22,24 @@ public class testBeam implements Serializable {
 
     public void buttonActionListener(ActionEvent actionEvent) {
         System.out.println("Cancel akcija odradjena!");   
+        log.append("Cancel! \n");
     }
 
     public void okButton(ActionEvent actionEvent) {
         System.out.println("OK akcija odradjena!");    
+        log.append("Ok \n");
+    }
+
+    public void clearButton(ActionEvent actionEvent) {
+        System.out.println("Clear akcija odradjena!");    
+        log = new StringBuilder();
+    }
+
+    public void setNatpis(String natpis) {
+        this.natpis = natpis;
+    }
+
+    public String getNatpis() {
+        return "Log: \n"+ log.toString();
     }
 }
